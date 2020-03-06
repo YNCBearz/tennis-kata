0220c15364533be2133a9bde178b428109d37831
YNCBearz <bear19930414@gmail.com>
2020-03-06 15:14:39 +0800

    refactor Game003

--- app/TennisGame/Game003.php
+++ app/TennisGame/Game003.php
@@ -27,6 +27,13 @@ class Game003
             return $this->lookup[$p1_score] . '-All';
         }
 
+        return $this->getNormalScore();
+    }
+
+    private function getNormalScore()
+    {
+        $p1_score = $this->p1_score;
+        $p2_score = $this->p2_score;
         return $this->lookup[$p1_score] . '-' . $this->lookup[$p2_score];
     }
 
