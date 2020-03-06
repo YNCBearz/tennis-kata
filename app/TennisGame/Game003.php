4c9feb471b233e78b764ddc33020b978a88f053e
YNCBearz <bear19930414@gmail.com>
2020-03-06 15:03:26 +0800

    refactor Game003

--- app/TennisGame/Game003.php
+++ app/TennisGame/Game003.php
@@ -3,20 +3,25 @@ namespace App\TennisGame;
 
 class Game003
 {
-    private $p1_score;
+    private $p1_score = 0;
+    private $p2_score = 0;
+
+    private $lookup = [
+        0 => 'Love',
+        1 => 'Fifteen',
+        2 => 'Thirty'
+    ];
 
     public function getScore()
     {
         $p1_score = $this->p1_score;
+        $p2_score = $this->p2_score;
 
-        if ($p1_score == 2) {
-            return 'Thirty-Love';
+        if ($p1_score == 0 && $p2_score == 0) {
+            return 'Love-All';
         }
 
-        if ($p1_score == 1) {
-            return 'Fifteen-Love';
-        }
-        return 'Love-All';
+        return $this->lookup[$p1_score] . '-' . $this->lookup[$p2_score];
     }
 
     public function player1Tally()
