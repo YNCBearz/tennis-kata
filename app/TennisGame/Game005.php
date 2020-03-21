d006be8473c99f04b2c3514d3821e352da5fdc2d
YNCBearz <bear19930414@gmail.com>
2020-03-21 23:58:46 +0800

    pass Love-All test

---
+++ app/TennisGame/Game005.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\TennisGame;
+
+class Game005
+{
+    public function getScore()
+    {
+        return 'Love-All';
+    }
+}
---
+++ tests/Unit/Game005Test.php
@@ -0,0 +1,29 @@
+<?php
+namespace Tests\Unit;
+
+use App\TennisGame\Game005;
+use PHPUnit\Framework\TestCase;
+
+class Game005Test extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->game = new Game005();
+    }
+
+    /**
+     * @test
+     */
+    public function getScore_LoveAll()
+    {
+        //Arrange
+        $expected = 'Love-All';
+
+        //Act
+        $actual = $this->game->getScore();
+
+        //Assert
+        $this->assertEquals($expected, $actual);
+    }
+
+}
