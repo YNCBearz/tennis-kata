55139c3e77f6debfe035dfe5be3b96040e5a24fc
YNCBearz <bear19930414@gmail.com>
2020-05-03 01:15:35 +0800

    refactor

--- app/TennisGame/Game007.php
+++ app/TennisGame/Game007.php
@@ -20,12 +20,11 @@ class Game007
             return $this->samePointScore();
         }
 
-        if ($this->firstPlayerPoint > 0 || $this->secondPlayerPoint > 0) {
-            if ($this->firstPlayerPoint > 3 && $this->firstPlayerPoint > $this->secondPlayerPoint) {
-                return 'FirstPlayer Adv';
-            }
-            return $this->normalScore();
-        };
+        if ($this->firstPlayerPoint > 3 && $this->firstPlayerPoint > $this->secondPlayerPoint) {
+            return 'FirstPlayer Adv';
+        }
+
+        return $this->normalScore();
     }
 
     private function normalScore()
