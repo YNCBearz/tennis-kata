8cb29877347da1533a174919a931c9551486a01e
YNCBearz <bear19930414@gmail.com>
2020-05-05 23:39:57 +0800

    refactor

--- app/TennisGame/Game010.php
+++ app/TennisGame/Game010.php
@@ -5,10 +5,15 @@ class Game010
 {
     private $p1Point = 0;
 
+    private $lookup = [
+        0 => 'Love',
+        1 => 'Fifteen'
+    ];
+
     public function score()
     {
         if ($this->p1Point == 1) {
-            return 'Fifteen-Love';
+            return $this->lookup[$this->p1Point] . '-Love';
         }
         return 'Love-All';
     }
