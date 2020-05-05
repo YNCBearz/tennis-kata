3017659e53f9789fe2d27471806d5cc0288d375b
YNCBearz <bear19930414@gmail.com>
2020-05-06 00:02:44 +0800

    refactor

--- app/TennisGame/Game010.php
+++ app/TennisGame/Game010.php
@@ -16,18 +16,34 @@ class Game010
 
     public function score()
     {
-        if ($this->p1Point == $this->p2Point) {
+        if ($this->isSameScore()) {
             return $this->lookup[$this->p1Point] . '-All';
         }
 
-        if ($this->p1Point > 0) {
-            return $this->lookup[$this->p1Point] . '-Love';
+        if ($this->p1Point > 0 || $this->p2Point > 0) {
+            return $this->normalScore();
         }
+    }
 
-        if ($this->p2Point > 0) {
-            return 'Love-' . $this->lookup[$this->p2Point];
-        }
+    private function normalScore()
+    {
+        return $this->translatePlayer1Point() . '-' . $this->translatePlayer2Point();
+    }
 
+    private function translatePlayer1Point()
+    {
+        return $this->lookup[$this->p1Point];
+    }
+
+    private function translatePlayer2Point()
+    {
+        return $this->lookup[$this->p2Point];
+    }
+
+
+    private function isSameScore()
+    {
+        return $this->p1Point == $this->p2Point;
     }
 
     public function player1WinPoint($point = 1)
