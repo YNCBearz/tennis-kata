489f73745b43d9e4614e86b3237abf186ac736ff
YNCBearz <bear19930414@gmail.com>
2020-05-07 22:53:37 +0800

    refactor

---
+++ app/TennisGame/Game012.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\TennisGame;
+
+class Game012
+{
+    public function score()
+    {
+        return 'Love-All';
+    }
+}
---
+++ tests/Unit/Game012Test.php
@@ -0,0 +1,27 @@
+<?php
+namespace Tests\Unit;
+
+use App\TennisGame\Game012;
+use PHPUnit\Framework\TestCase;
+
+class Game012Test extends TestCase
+{
+    protected $game;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->game = new Game012();
+    }
+
+    public function testLoveAll()
+    {
+        $this->scoreShouldBe('Love-All');
+    }
+
+    private function scoreShouldBe($expected)
+    {
+        $actual = $this->game->score();
+        $this->assertEquals($expected, $actual);
+    }
+}
