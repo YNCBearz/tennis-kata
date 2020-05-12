c8af243cb6c57400c303ec47114a7d1b5cb0e51d
YNCBearz <bear19930414@gmail.com>
2020-05-12 21:59:23 +0800

    refactor

--- app/TennisGame/Game016.php
+++ app/TennisGame/Game016.php
@@ -18,19 +18,18 @@ class Game016
     public function score()
     {
 
-        if ($this->firstPlayerPoint == $this->secondPlayerPoint && $this->firstPlayerPoint == 2) {
-            return 'Thirty-All';
-        }
-
-        if ($this->firstPlayerPoint == $this->secondPlayerPoint && $this->firstPlayerPoint == 1) {
-            return 'Fifteen-All';
+        if ($this->isSamePoint()) {
+            return $this->lookup[$this->firstPlayerPoint] . '-All';
         }
 
         if ($this->firstPlayerPoint > 0 || $this->secondPlayerPoint > 0) {
             return $this->lookup[$this->firstPlayerPoint] . '-' . $this->lookup[$this->secondPlayerPoint];
         }
+    }
 
-        return 'Love-All';
+    private function isSamePoint()
+    {
+        return ($this->firstPlayerPoint == $this->secondPlayerPoint);
     }
 
     public function secondPlayerWinPoint($point = 1)
