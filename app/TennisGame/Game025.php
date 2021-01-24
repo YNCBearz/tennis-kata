807a01e1ed175f1b622fdd1931bc72df31352908
YNCBearz <bear19930414@gmail.com>
2021-01-24 17:57:33 +0800

    game 025

--- app/TennisGame/Game025.php
+++ app/TennisGame/Game025.php
@@ -14,6 +14,16 @@ class Game025
      */
     protected $secondPlayerPoint = 0;
 
+    /**
+     * @var string
+     */
+    protected $firstPlayer;
+
+    /**
+     * @var string
+     */
+    protected $secondPlayer;
+
     protected $lookup = [
         0 => 'Love',
         1 => 'Fifteen',
@@ -21,15 +31,66 @@ class Game025
         3 => 'Forty'
     ];
 
+    public function __construct($firstPlayer, $secondPlayer)
+    {
+        $this->firstPlayer = $firstPlayer;
+        $this->secondPlayer = $secondPlayer;
+    }
+
     public function score()
     {
         if ($this->firstPlayerPoint != $this->secondPlayerPoint) {
+            if ($this->isAnyPlayerGet4Point()) {
+                if ($this->isDiffMoreThan1Point()) {
+                    return $this->gameSetScore();
+                }
+
+                return $this->advScore();
+            }
+
             return $this->normalScore();
         }
 
-        return ($this->firstPlayerPoint == 3)
-            ? 'Deuce'
-            : $this->lookup[$this->firstPlayerPoint] . '-All';
+        return ($this->firstPlayerPoint >= 3)
+            ? $this->deuceScore()
+            : $this->samePointScore();
+    }
+
+    private function gameSetScore()
+    {
+        return $this->advPlayer() . ' Win';
+    }
+
+    private function isDiffMoreThan1Point()
+    {
+        return abs($this->firstPlayerPoint - $this->secondPlayerPoint) > 1;
+    }
+
+    private function advScore()
+    {
+        return $this->advPlayer() . ' Adv';
+    }
+
+    private function isAnyPlayerGet4Point()
+    {
+        return ($this->firstPlayerPoint >= 4 || $this->secondPlayerPoint >= 4);
+    }
+
+    private function advPlayer()
+    {
+        return ($this->firstPlayerPoint > $this->secondPlayerPoint)
+            ? $this->firstPlayer
+            : $this->secondPlayer;
+    }
+
+    private function deuceScore()
+    {
+        return 'Deuce';
+    }
+
+    private function samePointScore()
+    {
+        return $this->lookup[$this->firstPlayerPoint] . '-All';
     }
 
     private function normalScore()
--- tests/Unit/Game025Test.php
+++ tests/Unit/Game025Test.php
@@ -14,7 +14,7 @@ class Game025Test extends TestCase
 
     protected function setUp(): void
     {
-        $this->sut = new Game025();
+        $this->sut = new Game025('Bear', 'Lin');
     }
 
     public function testLoveAll()
@@ -70,5 +70,54 @@ class Game025Test extends TestCase
         $this->sut->firstPlayerWinPoint(3);
         $this->sut->secondPlayerWinPoint(3);
         $this->scoreShouldBe('Deuce');
+    }
+
+    public function testDeuceWhen4v4()
+    {
+        $this->sut->firstPlayerWinPoint(4);
+        $this->sut->secondPlayerWinPoint(4);
+        $this->scoreShouldBe('Deuce');
+    }
+
+    public function testFirstPlayerAdv()
+    {
+        $this->sut->firstPlayerWinPoint(4);
+        $this->sut->secondPlayerWinPoint(3);
+        $this->scoreShouldBe('Bear Adv');
+    }
+
+    public function testFirstPlayerAdvWhen5v4()
+    {
+        $this->sut->firstPlayerWinPoint(5);
+        $this->sut->secondPlayerWinPoint(4);
+        $this->scoreShouldBe('Bear Adv');
+    }
+
+    public function testSecondPlayerAdv()
+    {
+        $this->sut->firstPlayerWinPoint(3);
+        $this->sut->secondPlayerWinPoint(4);
+        $this->scoreShouldBe('Lin Adv');
+    }
+
+    public function testSecondPlayerAdvWhen4v5()
+    {
+        $this->sut->firstPlayerWinPoint(4);
+        $this->sut->secondPlayerWinPoint(5);
+        $this->scoreShouldBe('Lin Adv');
+    }
+
+    public function testFirstPlayerWin()
+    {
+        $this->sut->firstPlayerWinPoint(5);
+        $this->sut->secondPlayerWinPoint(3);
+        $this->scoreShouldBe('Bear Win');
+    }
+
+    public function testSecondPlayerWin()
+    {
+        $this->sut->firstPlayerWinPoint(2);
+        $this->sut->secondPlayerWinPoint(4);
+        $this->scoreShouldBe('Lin Win');
     }
 }
