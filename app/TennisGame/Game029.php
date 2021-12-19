789e4c7b8a356dc93e98781af4f243c26ce9d1da
YNCBearz <bear19930414@gmail.com>
2021-12-19 15:16:29 +0800

    pass Fifteen-Love test

--- app/TennisGame/Game029.php
+++ app/TennisGame/Game029.php
@@ -5,8 +5,19 @@ namespace App\TennisGame;
 class Game029
 {
 
+    protected int $firstPlayerPoint = 0;
+
     public function score()
     {
+        if ($this->firstPlayerPoint == 1) {
+            return 'Fifteen-Love';
+        }
+
         return 'Love-All';
+    }
+
+    public function firstPlayerWinPoint()
+    {
+        $this->firstPlayerPoint++;
     }
 }
