9d7e87cda8b9943a4fa41731deb54bc32cf2d41f
YNCBearz <bear19930414@gmail.com>
2022-01-09 16:33:15 +0800

    pass love all test

---
+++ app/TennisGame/Game033.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\TennisGame;
+
+class Game033
+{
+
+    public function score()
+    {
+        return 'Love All';
+    }
+}
---
+++ tests/Unit/Game033Test.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\TennisGame\Game033;
+use PHPUnit\Framework\TestCase;
+
+class Game033Test extends TestCase
+{
+    protected $sut;
+
+    /**
+     * @test
+     */
+    public function testLoveAll()
+    {
+        $this->scoreShouldBe('Love All');
+    }
+
+    public function scoreShouldBe($expected): void
+    {
+        $actual = $this->sut->score();
+        $this->assertEquals($expected, $actual);
+    }
+
+    protected function setUp(): void
+    {
+        $this->sut = new Game033();
+    }
+
+}
