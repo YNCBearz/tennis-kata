849ba99650bc7a7fb216b88c79b948b08fa82108
YNCBearz <bear19930414@gmail.com>
2022-01-16 12:21:02 +0800

    refactor

--- tests/Unit/Game034Test.php
+++ tests/Unit/Game034Test.php
@@ -7,15 +7,25 @@ use PHPUnit\Framework\TestCase;
 
 class Game034Test extends TestCase
 {
+    protected $sut;
+
+    public function scoreShouldBe($expected): void
+    {
+        $actual = $this->sut->score();
+        $this->assertEquals($expected, $actual);
+    }
+
+    protected function setUp(): void
+    {
+        $this->sut = new Game034();
+    }
+
     /**
      * @test
      */
     public function testLoveAll()
     {
-        $sut = new Game034();
-        $actual = $sut->score();
-        $expected = 'Love All';
-        $this->assertEquals($expected, $actual);
+        $this->scoreShouldBe('Love All');
     }
 
 }
