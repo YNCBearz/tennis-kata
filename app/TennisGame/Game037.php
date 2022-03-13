a5a11a1e015aa51fb7fe712de027417159fb9b79
YNCBearz <bear19930414@gmail.com>
2022-03-13 18:39:29 +0800

    pass deuce test

--- app/TennisGame/Game037.php
+++ app/TennisGame/Game037.php
@@ -20,6 +20,10 @@ class Game037
     public function score()
     {
         if ($this->isSamePoint()) {
+            if ($this->firstPlayerPoint >= 3) {
+                return 'Deuce';
+            }
+
             return $this->lookup[$this->firstPlayerPoint] . ' All';
         }
 
