4e61dd1c0212710dac4975777beed609cafe2a2c
YNCBearz <bear19930414@gmail.com>
2022-03-13 18:03:37 +0800

    refactor

--- app/TennisGame/Game036.php
+++ app/TennisGame/Game036.php
@@ -5,6 +5,10 @@ namespace App\TennisGame;
 class Game036
 {
     protected $firstPlayerPoint = 0;
+    protected $lookup = [
+        1 => 'Fifteen',
+        2 => 'Thirty',
+    ];
 
     public function __construct()
     {
@@ -12,11 +16,8 @@ class Game036
 
     public function score()
     {
-        if ($this->firstPlayerPoint == 1) {
-            return 'Fifteen Love';
-        }
-        if ($this->firstPlayerPoint == 2) {
-            return 'Thirty Love';
+        if ($this->firstPlayerPoint > 0) {
+            return $this->lookup[$this->firstPlayerPoint] . ' Love';
         }
 
         return 'Love All';
