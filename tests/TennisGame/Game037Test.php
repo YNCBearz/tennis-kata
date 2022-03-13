08bcdee23346de5678493308722d09ede4196f5d
YNCBearz <bear19930414@gmail.com>
2022-03-13 18:41:03 +0800

    pass bear adv test

--- tests/TennisGame/Game037Test.php
+++ tests/TennisGame/Game037Test.php
@@ -11,7 +11,7 @@ class Game037Test extends TestCase
 
     protected function setUp(): void
     {
-        $this->sut = new Game037();
+        $this->sut = new Game037('Bear');
     }
 
     /**
@@ -110,5 +110,15 @@ class Game037Test extends TestCase
     {
         $this->givenFirstPlayerWinPointTimes(3);
         $this->givenSecondPlayerWinPointTimes(3);
+    }
+
+    /**
+     * @test
+     */
+    public function testBearAdv()
+    {
+        $this->givenDeuce();
+        $this->sut->firstPlayerWinPoint();
+        $this->scoreShouldBe('Bear Adv');
     }
 }
